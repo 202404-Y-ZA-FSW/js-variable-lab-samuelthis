71ec8f1cb3fcb24e4257a0599b579940815579c5
samuel <31856641+samuelthis@users.noreply.github.com>
2024-05-02 08:13:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 // Write your code here
-
+const MyFirstName = "Samuel"
+const MyLastName = "Masuvhe"
+const myFullName = myFirstName + " " + myLastName;
+const myHobbies = ["reading", "writing", "cooking"];
+const currentStudent = true;
+const myDream = "My dream is to help people.";
+const bestFriend = "Khodani";
+const phoneNumber = "0646577889";
+const favoriteMeal = "Rice";
+const myBirthYear = "1995";
+const currentYear = new Date().getFullYear();
+const myAge = myBirthYear + " " + currentYear;
 // After your finish you can run the following command to start the server
 // node index.js
