76f1a848c18492909a45b9866a4838f72c7e36fa
samuel <31856641+samuelthis@users.noreply.github.com>
2024-05-02 08:26:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,5 +11,6 @@ const favoriteMeal = "Rice";
 const myBirthYear = "1995";
 const currentYear = new Date().getFullYear();
 const myAge = myBirthYear + " " + currentYear;
+console.log (MyFullName, myHobbies,)
 // After your finish you can run the following command to start the server
 // node index.js
